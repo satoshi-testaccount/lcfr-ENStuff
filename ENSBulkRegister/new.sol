c7f400ba9bce5a8eddd20f2a1419a0dbe6ca3569
lcfr-eth <62533238+lcfr-eth@users.noreply.github.com>
2022-04-27 17:29:55 -0400

    Update new.sol

--- ENSBulkRegister/new.sol
+++ ENSBulkRegister/new.sol
@@ -8,68 +8,68 @@ import "@ensdomains/ens-contracts/contracts/ethregistrar/ETHRegistrarController.
 
 contract ENSBulkRegister {
 
-    event log_named_string       (string key, string val);
+  event log_named_string       (string key, string val);
 
-    address baseRegistrarAddr = 0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5;
-    ETHRegistrarController controller = ETHRegistrarController(baseRegistrarAddr);
+  address ETHRegistrarControllerContract = 0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5;
+  ETHRegistrarController controller = ETHRegistrarController(ETHRegistrarControllerContract);
 
-    address owner;
+  address owner;
 
-    constructor() {
-        owner = msg.sender;
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner, "not owner.");
+    _;
+  }
+
+  function _updateETHRegistrarControllerContract(address _newRegistrar) external onlyOwner {
+    baseRegistrarAddr = _newRegistrar;  
+  }
+
+  function _updateContractOwner(address _newOwner) external onlyOwner {
+    owner = _newOwner;
+  }
+
+  function _emergencyWithdraw(address _payee) external onlyOwner {
+    (bool sent, bytes memory data) = _payee.call{value: address(this).balance}("");
+    require(sent, "Failed to send Ether");
+  }
+
+  function doCommitloop(bytes32[] memory _commitments) external {
+    for ( uint i = 0; i < _commitments.length; ++i ) {
+      controller.commit(_commitments[i]);
     }
+  }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
+  function doRegisterloop(string[] memory _names, bytes32[] memory _secrets, uint256 _duration) external payable { 
 
-    function updateRegistrarAddr(address _newRegistrar) external onlyOwner {
-        baseRegistrarAddr = _newRegistrar;
-    }
+    uint256 totalPrice = rentPriceLoop(_names, _duration);
 
-    function updateOwner(address _newOwner) external onlyOwner {
-        owner = _newOwner;
-    }
-
-    function emergencyWithdraw(address _payee) external onlyOwner {
-        (bool sent, bytes memory data) = _payee.call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
-    }
-
-    function doCommitloop(bytes32[] memory _commitments) external {
-        for ( uint i = 0; i < _commitments.length; ++i ) {
-            controller.commit(_commitments[i]);
-        }
-    }
-
-    function doRegisterloop(string[] memory _names, bytes32[] memory _secrets, uint256 _duration) external payable { 
-
-        uint256 totalPrice = rentPriceLoop(_names, _duration);
-
-        require(_names.length == _secrets.length, "names/secrets length mismatch");
+    require(_names.length == _secrets.length, "names/secrets length mismatch");
         
-        // could technically do this in the front end. remove totalPrice/rentPriceLoop. 
-        // calculate totalPrice in frontend to send as value. Test gas cost to decide.
+    // could technically do this in the front end. remove totalPrice/rentPriceLoop. 
+    // calculate totalPrice in frontend to send as value. Test gas cost to decide.
         
-        require(msg.value >= totalPrice, "Not enough Ether sent.");
+    require(msg.value >= totalPrice, "Not enough Ether sent.");
         
-        for( uint i = 0; i < _names.length; ++i ) {
-            uint price = controller.rentPrice(_names[i], _duration);
-            controller.register{value: price}(_names[i], msg.sender, _duration, _secrets[i]);
-            emit log_named_string("NameRegistered: ", _names[i]);
-        }
-        
-        // check if any extra eth and refund user.
-        
+    for( uint i = 0; i < _names.length; ++i ) {
+      uint price = controller.rentPrice(_names[i], _duration);
+      controller.register{value: price}(_names[i], msg.sender, _duration, _secrets[i]);
+      emit log_named_string("NameRegistered: ", _names[i]);
     }
+        
+    // check if any extra eth and refund user.
+        
+  }
 
-    function rentPriceLoop(string[] memory _name, uint256 _duration) public view returns(uint total) {
-        uint256 total;
-        for (uint i = 0; i < _name.length; ++i {
-            total += controller.rentPrice(_name[i], _duration);
-        }
-        return total;
+  function rentPriceLoop(string[] memory _name, uint256 _duration) public view returns(uint total) {
+    uint256 total;
+    for (uint i = 0; i < _name.length; ++i {
+      total += controller.rentPrice(_name[i], _duration);
     }
+    return total;
+  }
 
 }
